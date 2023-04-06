a86038673ce33b77f09aa58ed56d46109b1e69cb
zenia banerjee <zenia@itobuz.com>
2023-04-06 15:15:47 -0700

    note app updated

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,15 +1,18 @@
 const noteName = document.getElementById("noteName");
 const noteContent = document.getElementById("noteContent");
-const button = document.getElementById("add-btn");
 let note = document.getElementById("note");
 let updateData = document.getElementById("updateData");
+let createNote = document.getElementById("createNote");
 let result = null;
-document.querySelector("#add-btn").addEventListener("click", () => {
+
+document.querySelector("#plusBtn").addEventListener("click", () => {
+  createNote.style.display = "flex ";
+});
+document.querySelector("#addBtn").addEventListener("click", () => {
   submitNotes();
 });
 
 function submitNotes(event) {
-  // event.preventDefault()
   fetch("http://localhost:3000/addnote", {
     mode: "cors",
     method: "POST",
@@ -26,16 +29,13 @@ function submitNotes(event) {
       noteName.value = null;
       noteContent.value = null;
       readNotes();
-      alert("Note added");
     })
     .catch((error) => {
       console.log(error);
     });
-  
 }
 
 async function readNotes() {
-  // event.preventDefault()
   let response = await fetch("http://localhost:3000/notes")
     .then(function (res) {
       return res.json();
@@ -51,8 +51,10 @@ async function readNotes() {
         <div class = "item">
             <h2 class = "list-name" id = "list-name">${response.data[i].name}</h2>
             <p class = "list-item" id = "list-item">${response.data[i].content}</p>
-            <button class = "btn-color update" onClick="updateItem('${response.data[i].name}')">Update</button>
-            <button class = "btn-color delete" onClick="deleteItem('${response.data[i]._id}')">Delete</button>
+            <div class = "d-flex flex-row gap-2">
+            <button class = "buttons update" onClick="updateItem('${response.data[i].name}')">Update</button>
+            <button class = "buttons delete" onClick="deleteItem('${response.data[i]._id}')">Delete</button>
+            </div>
         </div>
         `;
     note.innerHTML += notesTaken;
@@ -61,6 +63,7 @@ async function readNotes() {
 readNotes();
 
 function deleteItem(noteId) {
+  console.log("hi");
   const response = fetch("http://localhost:3000/deletenote", {
     mode: "cors",
     method: "DELETE",
@@ -72,27 +75,38 @@ function deleteItem(noteId) {
     }),
   })
     .then(() => {
-      alert("Note deleted");
+      readNotes();
     })
     .catch((error) => {
       console.log(error);
     });
-  readNotes();
 }
 
 function updateItem(noteName) {
   let update = "";
   update = `
-        <div id = "updateDiv" class = "update-data">
-        <input id = "newName" class="new-name" type="text" placeholder="What name do you want to give?">
-        <input id = "newContent" class="new-content" type="text" placeholder="update your content">
-            <button class = "btn-color update" onClick="updateContent('${noteName}')">Update</button>
-        </div>
+  <div id="updateDiv" class=" flex-column w-100 p-4 justify-content-center align-items-center update-data">
+    <input
+      id="newName"
+      class="p-2 w-75 m-2 new-name"
+      type="text"
+      placeholder="What name do you want to give?"
+    />
+    <textarea
+      class="p-2 w-75 m-2 new-content"
+      name="newContent"
+      class="new-content"
+      id="newContent"
+        cols="40"
+        rows="5"
+        placeholder="update your content"
+    ></textarea>
+    <button class = "p-1 m-2 w-50 buttons update" onClick="updateContent('${noteName}')">Update</button>
+</div>
         `;
   updateData.innerHTML += update;
-  
 }
-function updateContent(noteName){
+function updateContent(noteName) {
   let newNoteName = document.getElementById("newName").value;
   let newNoteContent = document.getElementById("newContent").value;
   const response = fetch("http://localhost:3000/updatenote", {
@@ -108,13 +122,11 @@ function updateContent(noteName){
     }),
   })
     .then(() => {
-      readNotes();
       let updateDiv = document.getElementById("updateDiv");
       updateDiv.style.display = "none";
-      alert("Note updated");
+      readNotes();
     })
     .catch((error) => {
       console.log(error);
     });
-  
 }
